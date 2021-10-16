16bcb0eaf99d69ccb239263f4905df17c6969e06
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-10-16 19:09:22 -0300

    instalado webrtc-adapter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import adapter from "webrtc-adapter";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/global.scss";
+
+console.debug(
+  "webrtc-adapter, browser and version",
+  adapter.browserDetails.browser,
+  adapter.browserDetails.version
+);
 
 Vue.config.productionTip = false;
 
