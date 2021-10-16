55ff368f9c9c690d26d25c85e6d4fe3977063409
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-10-16 14:57:39 -0300

    Adicionada navegação

--- src/modules/site/pages/Home.vue
+++ src/modules/site/pages/Home.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>Home</div>
+  <div>
+    <h1>Home</h1>
+    <ul>
+      <li>
+        <router-link to="/home">Home (Login)</router-link>
+      </li>
+
+      <li>
+        <router-link to="/chat">Chat</router-link>
+      </li>
+
+      <li>
+        <router-link to="/video">Videoconferência</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
