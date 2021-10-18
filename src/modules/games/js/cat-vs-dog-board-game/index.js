ff0cef833b6003084623aa6483dd1264aaa2d025
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-10-18 17:58:38 -0300

    Desenhando mapa

--- src/modules/games/js/cat-vs-dog-board-game/index.js
+++ src/modules/games/js/cat-vs-dog-board-game/index.js
@@ -42,7 +42,7 @@ class Game {
     this._onFPS = _onFPS;
 
     // Iniciar componentes
-    this._board = new Board(0, 0, this._gameWidth, this._gameHeight);
+    this._board = new Board(this._gameWidth, this._gameHeight);
 
     // Sucesso
     console.info("Game.version", Game.version);
@@ -98,23 +98,82 @@ class Game {
   }
 }
 
-class Board {
-  // path = null;
+// class BoardSquare {}
 
-  constructor(x, y, width, height) {
-    width, height;
-    this._x = x;
-    this._y = y;
+class Board {
+  constructor(width, height) {
+    // O board sempre será quadrado
+    // O tamanho dele será menor lado
+    // E sempre será calculado o centro
+    const padding = 80;
+    const size = width < height ? width - padding : height - padding;
+    const center = {
+      x: Math.ceil(width / 2),
+      y: Math.ceil(height / 2),
+    };
+
+    this._size = size;
+    this._center = center;
+  }
+
+  _getBoardPoints(centerX, centerY, size) {
+    const degToRad = (degrees) => (Math.PI / 180) * degrees;
+    const points = [];
+
+    // Primeiro ponto: central
+    points.push({
+      x: centerX,
+      y: centerY,
+    });
+
+    // Demais pontos sobre o circulo
+    const nPoints = 5;
+    const firstPointAngleDeg = -90;
+    for (let i = 0; i < nPoints; i++) {
+      const stepDeg = 360 / nPoints;
+      const angleDeg = i * stepDeg + firstPointAngleDeg;
+      const angleRad = degToRad(angleDeg);
+      const radius = size / 2;
+      points.push({
+        x: centerX + Math.cos(angleRad) * radius,
+        y: centerY + Math.sin(angleRad) * radius,
+      });
+    }
+
+    return points;
   }
 
   /**
    * @param {CanvasRenderingContext2D} context
    */
   draw(context) {
-    this.path = new Path2D();
-    this.path.arc(this._x++, this._y++, 50, 0, 2 * Math.PI);
-    context.stroke(this.path);
-    // context.drawImage()
+    const outline = new Path2D();
+    const squares = new Path2D();
+    // Desenha o circulo do tabuleito
+    outline.arc(this._center.x, this._center.y, this._size / 2, 0, 2 * Math.PI);
+
+    const points = this._getBoardPoints(this._center.x, this._center.y, this._size);
+    const rectSize = 40;
+    for (const p of points) {
+      squares.rect(p.x - rectSize / 2, p.y - rectSize / 2, rectSize, rectSize);
+    }
+
+    outline.moveTo(points[0].x, points[0].y);
+    outline.lineTo(points[3].x, points[3].y);
+    outline.moveTo(points[0].x, points[0].y);
+    outline.lineTo(points[4].x, points[4].y);
+
+    context.lineWidth = 4;
+    context.fillStyle = "rgba(0,0,0,1)";
+    context.stroke(outline);
+    context.fill(squares);
+  }
+
+  getCoordinates() {
+    // return {
+    //   x: this._x,
+    //   y: this._y,
+    //  }
   }
 }
 
